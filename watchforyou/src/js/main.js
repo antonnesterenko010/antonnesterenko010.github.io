e9a0c9f70f714058aa2420bbbd2c33d640ff6236
Anton <anton.nesterenko.010@gmail.com>
2019-12-12 13:01:16 +0200

    Сверстана и адаптирована шапка

--- watchforyou/src/js/main.js
+++ watchforyou/src/js/main.js
@@ -14,8 +14,34 @@ $(document).ready(function () {
     prevArrow: ('.arrows-left'),
     nextArrow: ('.arrows-right')
   })
+  // fixed menu init
   $('.greetings-list__button').click(function(){
     $(this).toggleClass('greetings-list__button-active');
     $('.greetings-list').toggleClass('greetings-list__active');
   })
+  // modal form init
+  $('.navbar-button').click(function(){
+    $('.modal').fadeIn();
+  })
+  // button-close init
+  $('.button-close').click(function(){
+    $('.modal').fadeOut();
+  })
+  $(document).mouseup(function (e) {
+  var close = $('.modal-dialog');
+  if (e.target != close[0] && close.has(e.target).length === 0) {
+    $('.modal').fadeOut();
+  };
+  $('body').keydown(function (event) {
+    if (event.which == 27) {
+      $('.modal').fadeOut();
+    }
+  });
+});
+// burger menu init
+$('.navbar-button__burger').click(function(){
+  $(this).toggleClass('navbar-button__burger__active');
+  $('.navbar-list__item').toggleClass('navbar-list__item_active');
+  $('.navbar-list').toggleClass('navbar-list_active');
+})
 })
