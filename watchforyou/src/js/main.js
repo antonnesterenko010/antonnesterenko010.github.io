0ad2c37e27cc0d20f966b382e1d6962f6774629f
Anton <anton.nesterenko.010@gmail.com>
2019-12-25 15:30:16 +0200

    Завершена статическая верстка сайта. Дальше все изменения на php

--- watchforyou/src/js/main.js
+++ watchforyou/src/js/main.js
@@ -1,3 +1,7 @@
+// jquery masked input 
+jQuery(function ($) {
+  $(".form__input__tel").mask("+38 (999) 999-99-99");
+});
 $(document).ready(function () {
   // float scroll
   $('a[href^="#"]').click(function () {
@@ -6,6 +10,25 @@ $(document).ready(function () {
       scrollTop: $(target).offset().top
     }, 1500);
   });
+  // backtop scroll init
+  $("#back-top").hide();
+  $(function () {
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+        $('#back-top').fadeIn();
+      } else {
+        $('#back-top').fadeOut();
+      }
+    });
+  $('#back-top a').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+});
+    });
+
+
   // slick slider init
     $('.product-slider-main').slick({
       slidesToShow: 1,
@@ -96,5 +119,93 @@ $('.navbar-button__burger').click(function(){
   $(this).toggleClass('navbar-button__burger__active');
   $('.navbar-list__item').toggleClass('navbar-list__item_active');
   $('.navbar-list').toggleClass('navbar-list_active');
-})
+});
+// jquery validate init
+$("#modal-form").validate({
+    rules: {
+      user_name: {
+        required: true
+      },
+      user_tel: {
+        required: true
+      }
+    },
+    messages: {
+      user_name: {
+        required: "Пожалуйста, укажите ваше имя"
+      },
+      user_tel: {
+        required: "Для связи с вами нам необходим ваш мобильный номер!"
+      }
+    },
+    submitHandler: function (form) {
+      $.ajax({
+        url: 'mail.php',
+        type: 'POST',
+        data: $('#modal-form').serialize(),
+        success: function (data) {
+          $('form input[type="text"], form textarea').val('');
+          $('.modalty').fadeIn();
+        }
+      })
+    }
+  });
+$("#main-form").validate({
+  rules: {
+    user_name: {
+      required: true
+    },
+    user_tel: {
+      required: true
+    },
+    user_email: {
+      email: true,
+    }
+  },
+  messages: {
+    user_name: {
+      required: "Пожалуйста, укажите ваше имя"
+    },
+    user_tel: {
+      required: "Введите корректный номер!"
+    },
+    user_email: {
+      email: "Пожалуйста введите корректный e-mail"
+    }
+  },
+  submitHandler: function (form) {
+    $.ajax({
+      url: 'mail.php',
+      type: 'POST',
+      data: $('#main-form').serialize(),
+      success: function (data) {
+        $('form input[type="text"], form textarea').val('');
+        $('.modalty').fadeIn();
+      }
+    })
+  },
+});
+$("#product-form").validate({
+  rules: {
+    user_tel: {
+      required: true
+    }
+  },
+  messages: {
+    user_tel: {
+      required: "Для связи с вами нам необходим ваш мобильный номер!"
+    }
+  },
+  submitHandler: function (form) {
+    $.ajax({
+      url: 'mail.php',
+      type: 'POST',
+      data: $('#product-form').serialize(),
+      success: function (data) {
+        $('form input[type="text"], form textarea').val('');
+        $('.modalty').fadeIn();
+      }
+    })
+  }
+});
 })
