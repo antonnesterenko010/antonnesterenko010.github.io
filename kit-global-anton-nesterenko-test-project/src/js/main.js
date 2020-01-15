373bdaa7cd954691e846b9d0f483528f71fb4a12
Anton <anton.nesterenko.010@gmail.com>
2020-01-16 00:55:08 +0200

    Сверстан header, menu, в процессе content

---
+++ kit-global-anton-nesterenko-test-project/src/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $('.slider-list').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: ('.slider-arrows__left'),
+    nextArrow: ('.slider-arrows__right'),
+  });
+});
