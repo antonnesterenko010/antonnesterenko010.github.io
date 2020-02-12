60cb851a9a9766afd33250114b7275496e60a2e4
Anton <anton.nesterenko.010@gmail.com>
2020-02-12 20:32:48 +0200

    Создан калькулятор на bootstrap с помощью jquery без оформления стилей

---
+++ state-road-calc/src/js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  $('#calc').change(function(){
+    $tattooLenght = $('input#t1').val();
+    $tattooWidth = $('input#t2').val();
+    $tattooColor = $('select#t3 option:selected').attr('data-color');
+    $tattooStyle = $('select#t4 option:selected').attr('data-style');
+    // if ($tattooColor == 1) {
+    //   $('.qwe').hide();
+    // } else {
+    //   $('.qwe').show();
+    // }
+
+    $price =  $tattooLenght * $tattooWidth * $tattooColor * $tattooStyle;
+
+    if ($price < 400) {
+      $('#btn-result').click(function () {
+        $('.sum2').text('400');
+      })
+    }
+    if ($price > 4000) {
+      $('#btn-result').click(function () {
+        $('.sum2').text('Ваша татуировка займёт более одного сеанса. Стоимость сеанса 2000-3000 грн, в зависимости от сложности работы. В ближайшее время наш менеджер свяжется с Вами и предоставит детальную консультацию. Спасибо');
+        $('.sum1').hide();
+      })
+    }
+  })
+
+  $('#btn-result').click(function(){
+    $('.sum').text($price);
+  })
+
+})
