75cc38bf99ba039578e0d0d026908bab0692ea48
Anton <anton.nesterenko.010@gmail.com>
2020-03-09 12:55:08 +0200

    Добавил плавность прокрутки, валидацию формы, number mask input, stats counter

--- marakas-test-project/src/js/main.js
+++ marakas-test-project/src/js/main.js
@@ -11,58 +11,10 @@ $('.header-menu__btn').click(function(e) {
   }, function () {
     $('.header-logo').css('position', '').css('left', '750px');
   });
-  // $('.header-menu__btn').css('right', '230px')
-  // $('.header-menu__btn').css('position', 'fixed').css('right', '225px');
+ 
 });
 
-// if ($('#header-menu__btn').hasClass('header-menu__btn_active')){
-//   $('header-logo').css('position', 'fixed');
 
-//   $('header-logo').css('left', '750px');
-// }
-// $(function() {
-//   menu_top = $('.header-menu').offset().top;      
-//   $(window).scroll(function () {             
-//     if ($(window).scrollTop() > menu_top ) {  
-//       if ($('.header-menu').css('position') != 'fixed') {  
-//         $('.header-menu').addClass('header-menu_active');
-
-//         $('.header-menu').css('position','fixed');  
-//         $('.header-menu').css('top', '0');
-//         $('.header-menu').css('width', '100%');
-//         $('.header-menu').css('background', 'rgba(0,0,0,0.7)')
-//         $('.header-menu').css('padding-top', '20px'); 
-//         $('.header-logo').css('position', 'fixed');  
-//         $('.header-logo').css('left', '230px');
-
-//         // $('.header-logo__name').removeClass('header-logo__name');
-//         // $('.header-logo__name').addClass('header-logo__name_active');
-//         $('.header-logo__name').css('display', 'block');
-//         $('.header-logo__name').css('position', 'fixed');
-//         $('.header-logo__name').css('top', '30px');
-//         $('.header-logo__name').css('left', '290px');
-//         $('.header-menu__btn').css('right', '-1265px');
-//       }
-//     } else {                                 
-//       if ($('.header-menu').css('position') == 'fixed') {  
-//         $('.header-menu').css('position','');
-//         $('.header-menu').css('width', '');
-//         $('.header-menu').css('background', 'rgba(0,0,0,0.0)')
-//         $('.header-menu').css('top', '');
-//         $('.header-logo').css('position', '');  
-//         // $('.header-logo').css('left', '0px');
-//         $('.header-menu__btn').css('right', '');
-//         $('.header-logo__name').css('display', 'none');
-        
-//       }
-//       // if ($('.header-menu__btn').toggleClass('header-menu__btn_active')){
-//       //   $('.header-logo').css('position', 'fixed').css('left', '750px')
-
-//       // }
-//     }
-    
-//   });
-// });
 $(function () {
   menu_top = $('.header-menu').offset().top;
   $(window).scroll(function () {
@@ -89,4 +41,119 @@ $(document).ready(function(){
     dots: true,
     dotsClass: 'slider-dots'
   })
+})
+
+$('a[href^="#"]').click(function () {
+  var target = $(this).attr('href');
+
+  $('.header-menu__btn').removeClass('header-menu__btn_active');
+
+  $('.header-logo').removeClass('header-logo_active');
+  $('.menu-section').fadeOut();
+  $('html, body').animate({
+    scrollTop: $(target).offset().top
+  }, 1500);
+});
+
+$('.stats-section').hover(function () {
+  $('.stats-item__counter').each(function () {
+    $(this).prop('Counter', 0).animate({
+      Counter: $(this).text().replace(',', '')
+    }, {
+      duration: 1500,
+      easing: 'swing',
+      step: function (now) {
+        $(this).text(Math.ceil(now).toLocaleString('en'));
+      },
+
+    });
+  });
+}, function () {
+  
+});
+// $(function () {
+//   menu_stats = $('.hero-section').offset().top;
+//   $(window).scroll(function () {
+//     if ($(window).scrollTop() > menu_stats) {
+
+
+//       $('.stats-item__counter').each(function () {
+//         $(this).prop('Counter', 0).animate({
+//           Counter: $(this).text().replace(',', '')
+//         }, {
+//           duration: 1500,
+//           easing: 'swing',
+//           step: function (now) {
+//             $(this).text(Math.ceil(now).toLocaleString('en'));
+//           },
+
+//         });
+//       });
+
+
+//     } else {
+//       $('.stats-item__counter').show();
+//     }
+
+//   });
+// });
+$(document).mouseup(function (e) {
+  $('body').keydown(function (event) {
+    if (event.which == 27) {
+
+      $('.header-menu__btn').removeClass('header-menu__btn_active');
+
+      $('.header-logo').removeClass('header-logo_active');
+      $('.menu-section').fadeOut();
+    }
+  });
+});
+$('input[name="phone"]').mask("+99 (999) 999-9999");
+$("#contact-form").validate({
+  rules: {
+    name: {
+      required: true
+    },
+    
+    email: {
+      required: true,
+      email: true
+    },
+    phone: {
+      required: true
+    },
+  },
+  messages: {
+    name: {
+      required: "Please, enter your name"
+    },
+
+    email: {
+      required: "Please, enter your email",
+      email: "Please, enter valid e-mail"
+    },
+    phone: {
+      required: "Please, enter your phone number"
+    }
+  },
+  submitHandler: function (form) {
+    $.ajax({
+      url: 'mail.php',
+      type: 'POST',
+      data: $('#contact-form').serialize(),
+      success: function (data) {
+        $('form input[type="text"], form textarea').val('');
+        alert('Thank you for sending your information!<br> We will contact you soon!');
+      },
+    })
+  },
+  invalidHandler: function (form) {
+    $.ajax({
+      success: function (data) {
+        $('input[name="name"]').css('border', '1px solid #E81B44');
+        $('input[name="email"]').css('border', '1px solid #E81B44');
+        $('input[name="phone"]').css('border', '1px solid #E81B44');
+      }
+    })
+  }
 })
