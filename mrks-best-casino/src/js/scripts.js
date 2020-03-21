cf1287e756364372999a0035d01de0db1c3b3178
Anton <anton.nesterenko.010@gmail.com>
2020-03-22 00:02:50 +0200

    Адаптировал строку поиска

--- mrks-best-casino/src/js/scripts.js
+++ mrks-best-casino/src/js/scripts.js
@@ -4,11 +4,15 @@ $('.header-burger').on('click', function (e) {
 });
 $('.header-search').click(function(e){
   e.preventDefault;
+  $(this).toggleClass('header-search_active');
   $('.header-search__input').toggleClass('header-search__input_active');
   $('.header-search__span').toggleClass('header-search__span_active');
+  $('.search-wrap').toggleClass('search-wrap_active');
+  $('.header-search__2').toggleClass('header-search__2_active');
 });
 $('.header-search__span').click(function(e){
   e.preventDefault;
-  $('.header-search__span').removeClass('header-search__span_active');
+  $(this).removeClass('header-search__span_active');
+  $('.header-search').removeClass('header-search_active');
   $('.header-search__input').removeClass('header-search__input_active');
 });
