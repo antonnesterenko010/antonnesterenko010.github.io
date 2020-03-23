898ee8f5324156cdfded0a8478ab5020e75483b5
Anton <anton.nesterenko.010@gmail.com>
2020-03-23 10:18:33 +0200

    Внесены правки в header-search

--- mrks-best-casino/src/js/scripts.js
+++ mrks-best-casino/src/js/scripts.js
@@ -1,18 +1,28 @@
 $('.header-burger').on('click', function (e) {
-  e.preventDefault;
+  e.preventDefault();
   $(this).toggleClass('header-burger_active');
 });
 $('.header-search').click(function(e){
-  e.preventDefault;
+  e.preventDefault();
   $(this).toggleClass('header-search_active');
   $('.header-search__input').toggleClass('header-search__input_active');
-  $('.header-search__span').toggleClass('header-search__span_active');
+  $('.header-search__close').toggleClass('header-search__close_active');
   $('.search-wrap').toggleClass('search-wrap_active');
   $('.header-search__2').toggleClass('header-search__2_active');
 });
-$('.header-search__span').click(function(e){
-  e.preventDefault;
-  $(this).removeClass('header-search__span_active');
+$('.header-search__close').click(function(e){
+  e.preventDefault();
+  $('.header-search__close').removeClass('header-search__close_active');
   $('.header-search').removeClass('header-search_active');
   $('.header-search__input').removeClass('header-search__input_active');
+});
+$(document).mouseup(function (e) {
+  var close = $('.header-search__input');
+  if (e.target != close[0] && close.has(e.target).length === 0) {
+    $('.header-search__input').removeClass('header-search__input_active');
+    $('.header-search').removeClass('header-search_active');
+    $('.search-wrap').removeClass('search-wrap_active');
+    $('.header-search__2').removeClass('header-search__2_active');
+    $('.header-search__close').removeClass('header-search__close_active');
+  }
 });
